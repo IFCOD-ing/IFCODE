5ffafd0842205db7b7fb17aae2f1cab99a18fb1e
woojeong kong <coolstevevc@gmail.com>
2022-03-02 00:59:20 +0900

    feat: update Main component with tab

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import MainNav from "../components/MainNav";
 import MainPane from "../components/MainPane";
 
 import PaneContainer from "../components/SplitPane/PaneContainer";
+import ContentContainer from "../components/ContentContainer";
+
+import TabList from "../components/tab/TabList";
+import Tab from "../components/tab/Tab";
+import TabPanel from "../components/tab/TabPanel";
 
 const MainWrapper = styled.div`
   display: flex;
@@ -13,15 +18,51 @@ const MainWrapper = styled.div`
 `;
 
 function Main() {
+  const [toggleState, setToggleState] = useState(0);
+
+  function handleTabClick(index) {
+    setToggleState(index);
+  }
+
   return (
     <MainWrapper>
       <MainNav />
       <MainPane>
         <PaneContainer viewType="vertical">
-          <div className="pane-content">code</div>
+          <ContentContainer>
+            <TabList>
+              <Tab
+                title="index.js"
+                index={0}
+                onClick={handleTabClick}
+                toggleState={toggleState}
+              />
+              <Tab
+                title="index.html"
+                index={1}
+                onClick={handleTabClick}
+                toggleState={toggleState}
+              />
+              <Tab
+                title="index.css"
+                index={2}
+                onClick={handleTabClick}
+                toggleState={toggleState}
+              />
+            </TabList>
+            <TabPanel index={0} toggleState={toggleState}>
+              index.js
+            </TabPanel>
+            <TabPanel index={1} toggleState={toggleState}>
+              index.html
+            </TabPanel>
+            <TabPanel index={2} toggleState={toggleState}>
+              index.css
+            </TabPanel>
+          </ContentContainer>
           <PaneContainer viewType="horizontal">
-            <div className="pane-content">view</div>
-            <div className="pane-content">console</div>
+            <ContentContainer>view</ContentContainer>
+            <ContentContainer>console</ContentContainer>
           </PaneContainer>
         </PaneContainer>
       </MainPane>
