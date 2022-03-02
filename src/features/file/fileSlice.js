a351c49b39815328c0501a7d533131fa885cb16a
woojeong kong <coolstevevc@gmail.com>
2022-03-02 20:51:35 +0900

    feat: add redux slice for add file

--- src/features/file/fileSlice.js
+++ src/features/file/fileSlice.js
@@ -1,21 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 import file from "../../file.json";
 
-import { createStructureId } from "../../helper/createStructureId";
+import { createStructureId, addFileById } from "../../helper/searchDfs";
 
 const initialState = {
   structure: createStructureId(file),
+  updateFile: "",
 };
 
 export const fileSlice = createSlice({
   name: "file",
   initialState,
   reducers: {
-    initializeFile: (state, action) => {},
+    registeredUpdateFile: (state, action) => {
+      const { id } = action.payload;
+      state.updateFile = id;
+    },
+    cancelUpdateFile: (state) => {
+      state.updateFile = "";
+    },
+    addFile: (state, action) => {
+      const { name } = action.payload;
+      addFileById(state.structure, state.updateFile, name);
+      state.updateFile = "";
+    },
   },
 });
 
-// Action creators are generated for each case reducer function
-export const { initializeFile } = fileSlice.actions;
+export const { registeredUpdateFile, cancelUpdateFile, addFile } =
+  fileSlice.actions;
 
 export default fileSlice.reducer;
