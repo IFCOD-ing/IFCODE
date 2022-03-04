959a5e720cdfaef5ae96549720a54500e3416f54
woojeong kong <coolstevevc@gmail.com>
2022-03-04 22:58:31 +0900

    feat: add file tree search logic

--- src/helper/searchDfs.js
+++ src/helper/searchDfs.js
@@ -40,6 +40,29 @@ function addFileById(file, id, name) {
   travelsalTree(file, id);
 }
 
+function addNewFileById(fileTree, folderId, name) {
+  const cloneFileTree = _.cloneDeep(fileTree);
+
+  function travelsalTree(data) {
+    if (!data) {
+      return;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === folderId) {
+        data[i].childrens.push({ type: "file", name, id: nanoid() });
+        return;
+      }
+
+      travelsalTree(data[i].childrens);
+    }
+  }
+
+  travelsalTree(cloneFileTree, folderId);
+
+  return cloneFileTree;
+}
+
 function findFileById(fileStructure, id) {
   let file;
 
@@ -63,4 +86,77 @@ function findFileById(fileStructure, id) {
   return file;
 }
 
-export { createStructureId, addFileById, findFileById };
+function updateFile(fileStructure, id, content) {
+  function travelsalTree(data) {
+    if (!data) {
+      return;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        data[i].content = content;
+        return;
+      }
+
+      travelsalTree(data[i].childrens, id, content);
+    }
+  }
+
+  travelsalTree(fileStructure, id, content);
+}
+
+function updateFileContent(fileTree, id, content) {
+  const cloneFileTree = _.cloneDeep(fileTree);
+
+  function travelsalTree(data, id, content) {
+    if (!data) {
+      return;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        data[i].content = content;
+        return;
+      }
+
+      travelsalTree(data[i].childrens, id, content);
+    }
+  }
+
+  travelsalTree(cloneFileTree, id, content);
+
+  return cloneFileTree;
+}
+
+function findRenderFile(fileStructure, name) {
+  let file;
+
+  function travelsalTree(data) {
+    if (!data) {
+      return;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].name === name) {
+        file = data[i].content;
+        return;
+      }
+
+      travelsalTree(data[i].childrens, name);
+    }
+  }
+
+  travelsalTree(fileStructure, name);
+
+  return file;
+}
+
+export {
+  createStructureId,
+  addFileById,
+  findFileById,
+  updateFile,
+  findRenderFile,
+  addNewFileById,
+  updateFileContent,
+};
