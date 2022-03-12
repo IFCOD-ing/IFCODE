a1d292fd0bafc268ee64445ec62624c5d268a472
woojeong kong <coolstevevc@gmail.com>
2022-03-13 07:26:11 +0900

    feat: modify style

--- src/components/Main/MainNav.js
+++ src/components/Main/MainNav.js
@@ -4,11 +4,24 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const NavWrapper = styled.div`
-  min-width: 400px;
-  height: 100%;
+  min-width: 300px;
+  height: 93vh;
   background-color: #151515;
   border-right: 5px solid #343434;
   color: white;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: #343434;
+  }
+  &::-webkit-scrollbar-track {
+    background: #151515;
+  }
 `;
 
 function MainNav({ children }) {
